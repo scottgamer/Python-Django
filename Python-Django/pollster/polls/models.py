6a8395f3f2030fb3d05df1ce193a66c9193d8c61
Richard Munoz <richosojason@msn.com>
2020-03-17 14:15:03 -0500

    initial django file structure added

--- Python-Django/pollster/polls/models.py
+++ Python-Django/pollster/polls/models.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  questionText = models.CharField(max_length=200)
+  pubDate = models.DateTimeField('date published')
+
+  def __str__(self):
+    return self.questionText
+
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choiceText = models.CharField(max_length=200) 
+  votes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.choiceText
+
+
+  
