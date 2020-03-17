33b2f7c4cfff1941a9be3054cc9d52685d82748b
Richard Munoz <richosojason@msn.com>
2020-03-17 14:30:43 -0500

    added admin.py

--- Python-Django/pollster/polls/admin.py
+++ Python-Django/pollster/polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['questionText']}),
+        ('Date Information', {'fields': ['pubDate'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
