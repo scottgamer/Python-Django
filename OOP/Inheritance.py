404df3b66bb3b269597824cb6795a585ab1d3bb6
Richard Munoz <richosojason@msn.com>
2020-03-23 12:08:01 -0500

    added class Book

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -19,7 +19,7 @@ class Dog(Animal):
 
     def bark(self):
         print('Woof')
-    
+
     def eat(self):
         print("Dog Eating")
 
@@ -30,3 +30,25 @@ my_dog.eat()
 my_dog.bark()
 
 # SPECIAL METHODS
+
+
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "Title: {}, Author: {}, Pages: {}".format(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("Book destroyed")
+
+
+book = Book("Python", "Richard", 200)
+print(book)
+print(len(book))
+del book
